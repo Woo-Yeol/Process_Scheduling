ea9b4c2e00cccdbae25f72404b70a85fec643f14
Woo-Yeol <king199276@gmail.com>
2021-04-22 23:19:19 +0900

    temp

--- Process_Scheduling/Scheduling/views.py
+++ Process_Scheduling/Scheduling/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from .models import Process,Processor,Spn
 
 # Create your views here.
 def index(request):
-    
+    print(request)
+    # result = Spn().mulitcore_processing()
 
     return render(request, 'index.html')
