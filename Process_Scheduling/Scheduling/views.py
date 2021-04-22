a34d45c77a8fd04d8fd3d944464e0acdab6f0ff1
Woo-Yeol <king199276@gmail.com>
2021-04-23 01:02:49 +0900

    Input Setting

--- Process_Scheduling/Scheduling/views.py
+++ Process_Scheduling/Scheduling/views.py
@@ -3,7 +3,28 @@ from .models import Process,Processor,Spn
 
 # Create your views here.
 def index(request):
-    print(request)
-    # result = Spn().mulitcore_processing()
-
+    if request.method == "POST":
+        if not request.POST["Process_name"] == "":
+            # choose_scheduling_type(request)
+            Spn(input_value(request)).mulitcore_processing()
+        return render(request, 'index.html')
     return render(request, 'index.html')
+
+def input_value(request):
+    return [
+        len(request.POST.getlist("Process_name")),
+        int(request.POST['processor-number']),
+        list(map(eval,request.POST['Burst_time'])),
+        list(map(eval,request.POST['Arrive_time']))]
+
+def choose_scheduling_type(request):
+    if request.POST["scheduling-type"] == "FCFS":
+        FCFS(input_value()).mulitcore_processing()
+    elif request.POST["scheduling-type"] == "RR":
+        RR(input_value()).mulitcore_processing()
+    elif request.POST["scheduling-type"] == "SPN":
+        Spn(input_value()).mulitcore_processing()
+    elif request.POST["scheduling-type"] == "SRTN":
+        SRTN(input_value()).mulitcore_processing()
+    else :
+        HRRN(input_value()).mulitcore_processing()
